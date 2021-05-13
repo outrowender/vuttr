72377f8fa96d3a2efd97f024f9259f4ec985f7ff
Wender Patrick <outrowender@gmail.com>
2021-05-13 20:00:50 -0300

    adicionado esqueeto da homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import "./App.css";
+import HomePage from "./pages/Home/HomePage";
 
 function App() {
   return (
-    <div className="App center">
-      <h2>This is a title</h2>
-      <p>This is a subtitle</p>
-      <button className="primary">Primary button</button>
-      <hr />
-      <button className="secondary">Secondary button</button>
-      <hr />
-      <label>Um label qualquer</label>
-      <hr/>
-      <div className="form-input">
-        <label>Meu input lindo</label>
-        <input type="text" placeholder="Encontre o que procura..." />
-        <p>Erro?</p>
-      </div>
-
-      <div className="form-input">
-        <label>Meu input feinho</label>
-        <input type="text" placeholder="Encontre o que procura..." />
-        <p className="error">Erro?</p>
-      </div>
+    <div className="App">
+      <HomePage />
     </div>
   );
 }
